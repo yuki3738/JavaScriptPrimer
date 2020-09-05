0478ef8a5ceacf644187e6e69dcfd754e651058c
yuki3738 <y.minamiya@gmail.com>
2020-09-05 23:08:13 +0900

    31.3 まで

--- Part2/Chapter31/nodecli/main.js
+++ Part2/Chapter31/nodecli/main.js
@@ -1,6 +1,14 @@
 const program = require('commander');
+const fs = require('fs');
 
 program.parse(process.argv);
-
 const filePath = program.args[0];
-console.log(filePath);
+
+fs.readFile(filePath, { encoding: 'utf8' }, (err, file) => {
+  if (err) {
+    console.error(err.message);
+    process.exit(1);
+    return;
+  }
+  console.log(file);
+});
