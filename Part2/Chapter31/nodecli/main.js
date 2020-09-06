7cd8819446e89345219417767e97a56eb3bbc28d
yuki3738 <y.minamiya@gmail.com>
2020-09-07 01:46:58 +0900

    31.4 まで

--- Part2/Chapter31/nodecli/main.js
+++ Part2/Chapter31/nodecli/main.js
@@ -1,8 +1,15 @@
 const program = require('commander');
 const fs = require('fs');
+const marked = require('marked');
 
+program.option('--gfm', 'GFMを有効にする');
 program.parse(process.argv);
 const filePath = program.args[0];
+
+const cliOptions = {
+  gfm: false,
+  ...program.opts(),
+};
 
 fs.readFile(filePath, { encoding: 'utf8' }, (err, file) => {
   if (err) {
@@ -10,5 +17,8 @@ fs.readFile(filePath, { encoding: 'utf8' }, (err, file) => {
     process.exit(1);
     return;
   }
-  console.log(file);
+  const html = marked(file, {
+    gfm: cliOptions.gfm,
+  });
+  console.log(html);
 });
