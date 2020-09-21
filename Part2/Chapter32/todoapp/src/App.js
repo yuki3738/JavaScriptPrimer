0e028e49e70819239627c0cdb29689f651937393
yuki3738 <y.minamiya@gmail.com>
2020-09-21 17:23:06 +0900

    32.3.2 まで

--- Part2/Chapter32/todoapp/src/App.js
+++ Part2/Chapter32/todoapp/src/App.js
@@ -1,6 +1,10 @@
-console.log("App.js: loaded");
 export class App {
-  constructor() {
-    console.log("App initialized");
+  mount() {
+    const formElement = document.querySelector("#js-form");
+    const inputElement = document.querySelector("#js-form-input");
+    formElement.addEventListener("submit", (event) => {
+      event.preventDefault();
+      console.log(`入力欄の値: ${inputElement.value}`);
+    });
   }
 }
