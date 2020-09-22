18880ffab1fdb7991e9c7a7e298e941e5ec0bab8
yuki3738 <y.minamiya@gmail.com>
2020-09-22 14:55:18 +0900

    32.3.5 まで

--- Part2/Chapter32/todoapp/src/App.js
+++ Part2/Chapter32/todoapp/src/App.js
@@ -1,10 +1,20 @@
+import { element } from "./view/html-util.js";
+
 export class App {
   mount() {
     const formElement = document.querySelector("#js-form");
     const inputElement = document.querySelector("#js-form-input");
+    const containerElement = document.querySelector("#js-todo-list");
+    const todoItemCountElement = document.querySelector("#js-todo-count");
+
+    let todoItemCount = 0;
     formElement.addEventListener("submit", (event) => {
       event.preventDefault();
-      console.log(`入力欄の値: ${inputElement.value}`);
+      const todoItemElement = element`<li>${inputElement.value}</li>`;
+      containerElement.appendChild(todoItemElement);
+      todoItemCount += 1;
+      todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+      inputElement.value = "";
     });
   }
 }
